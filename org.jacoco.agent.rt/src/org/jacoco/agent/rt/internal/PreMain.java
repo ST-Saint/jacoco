35a967053ad3c62109bfc23c4be43b2aeae7df73
Yayu Wang <st.saint.wyy@gmail.com>
2022-11-18 00:27:47 -0800

    chore: assign RuntimeData

--- org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/PreMain.java
+++ org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/PreMain.java
@@ -60,9 +60,10 @@ public final class PreMain {
 					.loadClass("edu.berkeley.cs.jqf.fuzz.jcc.JccGuidance");
 			System.out.println(
 					"[jacoco] load jcc guidance: " + (jccGuidance == null));
-			final Field jccRuntime = jccGuidance.getField("runtime");
-			System.out.println("[jacoco] get field: " + (jccRuntime == null));
-			jccRuntime.set(null, runtime);
+			final Field jccRuntimeData = jccGuidance.getField("runtimeData");
+			System.out
+					.println("[jacoco] get field: " + (jccRuntimeData == null));
+			jccRuntimeData.set(null, agent.getData());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
