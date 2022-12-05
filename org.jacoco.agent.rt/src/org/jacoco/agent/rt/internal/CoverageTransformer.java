30bed2043580189f6609262b7210da9f0149aeaa
Yayu Wang <st.saint.wyy@gmail.com>
2022-12-05 10:12:19 -0800

    chore: print instrumented classes

--- org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/CoverageTransformer.java
+++ org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/CoverageTransformer.java
@@ -177,6 +177,14 @@ public class CoverageTransformer implements ClassFileTransformer {
 			}
 		}
 
+		if (!classname.startsWith(AGENT_PREFIX) &&
+
+				includes.matches(classname) &&
+
+				!excludes.matches(classname)) {
+			System.out.println("[jacoco] instrument: " + classname);
+		}
+
 		return !classname.startsWith(AGENT_PREFIX) &&
 
 				includes.matches(classname) &&
